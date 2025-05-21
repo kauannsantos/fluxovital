de0d3d28fc07690637fc397ac44b697d4edf85c5
Paulouss <paulovinicius.pvlf@gmail.com>
2025-05-21 15:51:56 -0300

    Delete fluxovital directory

--- fluxovital/tipo_sangue.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS TipoSanguineo (
-    id_tipo INT AUTO_INCREMENT PRIMARY KEY,
-    tipo VARCHAR(5) NOT NULL
-);
-
-INSERT INTO TipoSanguineo (tipo) VALUES 
-('A+'), ('A-'), ('B+'), ('B-'), ('AB+'), ('AB-'), ('O+'), ('O-');
